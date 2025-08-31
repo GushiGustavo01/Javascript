9ebcd6911123b30eec72184974c0d387cdc2045e
Gustavo <temakeriagold@gmail.com>
2025-08-31 19:05:01 -0300

    Atualizaçõa

---
+++ Exercicios/Tabuada.js
@@ -0,0 +1,7 @@
+let valor = 2;
+
+for(let i = 1; i <= 10; i++){
+   
+    console.log(`2 x ${i} = ${valor * i}`);
+
+}
---
+++ Exercicios/array.js
@@ -0,0 +1,32 @@
+//for in array e conjunto.
+
+let nome1 = ["Gustavo", "Kelen", "Mia", "Kirion","Tapioca"];
+
+
+for(let i in nome1){
+    console.log(i);
+    console.log(nome1[i]);
+
+}
+
+let nome2= ["Gustavo", "Kelen", "Mia", "Kirion","Tapioca"];
+
+
+for(let i in nome2){
+    console.log(i);
+    console.log(nome2[i]);
+
+if(nome2[i] == "Gustavo" || nome2[i] == "gustavo"){
+    console.log(i);
+    
+}
+   
+}
+
+let conjunt = {nome: "Gustavo", idade: 25, cidade:"Recife", bairro:"Pina" };
+
+
+for(let chave in conjunt){
+console.log(chave);
+console.log(conjunt[chave]);
+}
