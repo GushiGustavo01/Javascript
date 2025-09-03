80766b3089484d4a9ea523f94eb433c40a4a1878
Gustavo <temakeriagold@gmail.com>
2025-09-03 20:58:42 -0300

    Estudos
    Atualização

--- Exercicios/Tabuada.js
+++ Exercicios/Tabuada.js
@@ -1,7 +1,24 @@
 let valor = 2;
+;
 
-for(let i = 1; i <= 10; i++){
+for( let i = 1; i <= 10; i++){
    
     console.log(`${valor} x ${i} = ${valor * i}`);
+
+
+}
+//for(let i = 1; 1 <= 10; 1+1){
+
+//console.log(`${2} x ${1}` = ${2 * 1}
+
+//}
+
+//for(let i = 1; 2 <= 10; 2+1){ //verdadeiro
+
+//console.log(`${2} x ${2}` = ${2 * 2} // mostre
+
+//}
+
+if(se ca){
 
 }
